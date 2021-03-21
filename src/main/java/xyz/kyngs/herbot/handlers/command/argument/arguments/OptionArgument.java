e8aa1ac9b7cf21cc4430658efc289b084388fa8c
kyngs <snezbeda@gmail.com>
2021-03-21 10:35:45 +0100

    Simplify parse code.

--- src/main/java/xyz/kyngs/herbot/handlers/command/argument/arguments/OptionArgument.java
+++ src/main/java/xyz/kyngs/herbot/handlers/command/argument/arguments/OptionArgument.java
@@ -10,17 +10,11 @@ public class OptionArgument extends AbstractArgument {
 
     @Override
     public Object parseArg(String arg, JDA jda) {
-        var ok = false;
         for (String s : optionsOrDescription) {
             if (s.contentEquals(arg)) {
-                ok = true;
-                break;
+                return arg;
             }
         }
-        if (ok) {
-            return arg;
-        } else {
-            throw new IllegalArgumentException();
-        }
+        throw new IllegalArgumentException();
     }
 }
